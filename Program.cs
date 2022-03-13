b97bb9a909ebddb96de6f46b32f2131d73802c27
M Usama Alvi <m.usamaalvi@gmail.com>
2022-03-13 11:31:23 +0000

    Adding comments and final changes.

--- Program.cs
+++ Program.cs
@@ -23,7 +23,7 @@ namespace CityDistance_Calculator
             }
             return minimiumDistance;
         }
-        public static int findIndexCities(string queryCity, List<string> cities){
+        public static int findIndexOfQueriedCity(string queryCity, List<string> cities){
             for(int i=0; i<cities.Count; i++){
                 if(queryCity == cities[i]){
                     return i;
@@ -32,24 +32,31 @@ namespace CityDistance_Calculator
             return -1;
         }
         public static List<string> closestStraightCity(List<string> c, List<int> x, List<int> y, List<string> q){
-            List<string> nearestCities = new List<string>();
-            
-            if(1<= c.Count && q.Count <= 100000 && c.Count == c.Distinct().Count()){
-                Dictionary<string, Dictionary<int,int>> queriedCitiesCoordinates = new Dictionary<string, Dictionary<int, int>>();
+            //Function Compexity(O(mn))
 
-                
+            List<string> nearestCities = new List<string>();
+            //Checking Constraints
+            if(1<= c.Count && q.Count <= 100000 && c.Count == c.Distinct().Count()){
+                //O(m)
                 for(int t = 0; t < q.Count; t++){
-                    int i = findIndexCities(q[t], c);
-                    if(c[i].Length >= 1 && c[i].Length <= 10){
+                    //Find index to access coordinates (O(n))
+                    int i = findIndexOfQueriedCity(q[t], c);
+
+                    //Checking Constraints
+                    if(c[i].Length >= 1 && c[i].Length <= 10 && 1 <= x[i] && x[i] <= 1000000000 && 1 <= y[i] && y[i] <= 1000000000 ){
                         List<string> minimumDistanceCities = new List<string>();
                         int minimiumDistance = int.MaxValue;
+
+                        //Finding minimum distance so we can group together minimum distance cities in order to sort them alphabatically later (O(n))
                         minimiumDistance = findMinimumDistance(c, x, y, i);
+
+                        //O(n)
                         for(int j = 0; j < c.Count; j++){
                             //Comparing same city
                             if(i==j){continue;}
                             
+                            //Grouping together minimum distance(calculated earlier) cities
                             if((x[i] == x[j] || y[i] == y[j]) && (manhattanDistance(x[i],x[j],y[i],y[j]) == minimiumDistance)){
-                                //minimiumDistance = manhattanDistance(x[i],x[j],y[i],y[j]);
                                 minimumDistanceCities.Add(c[j]);
                             }
                         }
@@ -57,6 +64,7 @@ namespace CityDistance_Calculator
                             nearestCities.Add("NONE");
                         }
                         else{
+                            //Sorting array alphabatically
                             string[] minimumCitiesArray = minimumDistanceCities.ToArray();
                             Array.Sort(minimumCitiesArray, (xCoordinate,yCoordinate) => String.Compare(xCoordinate, yCoordinate));
                             nearestCities.Add(minimumCitiesArray[0]);
@@ -67,10 +75,7 @@ namespace CityDistance_Calculator
                 }
 
             }
-            
-            
             return nearestCities;
-
         }
         static void Main(string[] args)
         {
